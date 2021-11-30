2b72c9e407d9356f001e3f5faee2ba9b74a2e383
romainemonkey <will_hahn@college.harvard.edu>
2021-11-29 20:42:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import json
 
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
 
 cid = "28832d036d4341d68dc4acea6dfc94b5"
 secret = "f810bd1fc2d3423d8009b28470cb7024"
@@ -12,3 +13,25 @@ os.environ['SPOTIPY_CLIENT_SECRET']= secret
 os.environ['SPOTIPY_REDIRECT_URI']='http://localhost:8888/callback'
 
 print("i love harvard university")
+
+username = ""
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+scope = 'user-top-read'
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+else:
+    print("Can't get token for", username)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_top_tracks(limit=50,offset=0,time_range='medium_term')
+    for song in range(50):
+        list = []
+        list.append(results)
+        with open('top50_data.json', 'w', encoding='utf-8') as f:
+            json.dump(list, f, ensure_ascii=False, indent=4)
+else:
+    print("Can't get token for", username)
