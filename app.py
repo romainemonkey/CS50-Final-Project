b64236c024a3f66921493fb4faaa149e536395a7
wallfeng <90158230+wallfeng@users.noreply.github.com>
2021-12-06 21:21:20 -0500

    updated website

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ db = SQLAlchemy(app)
 def index():
     return render_template('index.html')
 
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
